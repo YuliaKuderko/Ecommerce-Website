34772e18df4bf2960cdd4a8715555586e8055fb2
Yulia Kuderko <yulia2232@gmail.com>
2023-05-01 16:46:43 +0300

    created hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './header.css';
+import { Container } from 'reactstrap';
+import logo from '../../assets/images/eco-logo.png';
+import userIcon from '../../assets/images/user-icon.png';
+import { motion } from 'framer-motion';
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    const nav_links = [
+        {
+            path: 'home',
+            display: 'Home'
+        },
+        {
+            path: 'shop',
+            display: 'Shop'
+        },
+        {
+            path: 'cart',
+            display: 'Cart'
+        }
+    ]
+    return (
+        <header className='header'>
+            <Container>
+                    <div className="nav-wrapper">
+                        <div className="logo">
+                            <img src={logo} alt="logo" />
+                            <div>
+                                <h1>MyStore</h1>
+                            </div>
+                        </div>
+                        <div className="navigation">
+                            <ul className="menu">
+                                {nav_links.map((item, i) => (
+                                    <li className="nav-item" key={i}>
+                                        <NavLink to={item.path} className={(navClass) => navClass.isActive ? 'nav-active' : ''}>{item.display}</NavLink>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className='nav-icons'>
+                            <span className='fav-icon'>
+                                <i class="ri-heart-line"></i>
+                                <span className='badge'>2</span>
+                            </span>
+                            <span className='cart-icon'>
+                                <i class="ri-shopping-bag-line"></i>
+                                <span className='badge'>1</span>
+                            </span>
+                            <span><motion.img whileTap={{ scale: 1.1 }} src={userIcon} alt="user" /></span>
+                        </div>
+                        <div className='mobile-menu'>
+                            <span className='mobile-icon'><i class="ri-menu-line"></i></span>
+                        </div>
+                    </div>
+            </Container>
+        </header>
+    )
 }
 
 export default Header
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero-img.png';
+import '../styles/home.css'
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 function Home() {
+  const year = new Date().getFullYear();
+
   return (
-    <div>Home</div>
+    <Helmet title={"Home"}>
+      <section className='hero-section'>
+        <Container>
+          <Row>
+            <Col lg='6' md='6'>
+              <div className="hero-content">
+                <p className="hero-subtitle">Trending product in {year}</p>
+                <h2>Make Your Interior More Minimalistic & Modern</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam accusantium consequuntur eos perferendis error? Adipisci omnis ab accusantium suscipit, libero deserunt, quasi earum incidunt facere blanditiis repudiandae placeat quibusdam nihil.</p>
+                <motion.button whileTap={{scale:1.1}} className="shop-btn"><Link to='/shop'>SHOP NOW</Link></motion.button>
+              </div>
+            </Col>
+            <Col lg='6' md='6'>
+              <div className="hero-img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
