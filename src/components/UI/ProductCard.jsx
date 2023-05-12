de4a98ffaf670b59c6da8969950ac4ba5b221fa5
Yulia Kuderko <yulia2232@gmail.com>
2023-05-12 12:42:15 +0300

    changed Link on product cards

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -25,14 +25,16 @@ function ProductsCard({ item }) {
   return (
     <Col lg='3' md='4' className='mb-2'>
       <div className="product-item">
-
+      <Link to={`/shop/${item.id}`}>
         <div className="product-img">
           <motion.img whileHover={{ scale: 0.9 }} src={item.imgUrl} alt="" />
         </div>
         <div className='p-2 product-info text-start'>
-          <h3 className='product-name'><Link to={`/shop/${item.id}`}>{item.productName}</Link></h3>
+          <h3 className='product-name'>{item.productName}</h3>
           <span>{item.category}</span>
         </div>
+      </Link>
+        
         <div className="product-card-bottom d-flex align-items-center justify-content-between p-2">
           <span className='price'>${item.price}</span>
           <span onClick={addToCart}><i class="ri-add-line"></i></span>
