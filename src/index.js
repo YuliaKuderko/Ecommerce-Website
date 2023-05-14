102ead8cef57b8338f1d0b61a1a4bca8f313133a
Yulia Kuderko <yulia2232@gmail.com>
2023-05-15 00:15:05 +0300

    changed color of toast

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
     <BrowserRouter>
       <Provider store={store}>
         <ToastContainer
-          theme="light"
+          theme="dark"
           position="top-left"
           autoClose={3000}
           transition={Flip}
