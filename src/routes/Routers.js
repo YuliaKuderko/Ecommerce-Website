5d38ae1840a51513143df237c788512e5d7fdd94
Yulia Kuderko <yulia2232@gmail.com>
2023-05-15 17:06:41 +0300

    added user authentication

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -6,19 +6,21 @@ import Cart from '../pages/Cart';
 import ProductDetails from '../pages/ProductDetails';
 import Shop from '../pages/Shop';
 import Checkout from '../pages/Checkout';
-
+import ProtectedRoute from './ProtectedRoute';
 
 function Routers() {
   return (
     <Routes>
-      <Route path='/' element={<Navigate to='home'/>} />
-      <Route path='home' element={<Home/>}/>
-      <Route path='shop' element={<Shop/>}/>
-      <Route path='shop/:id' element={<ProductDetails/>}/>
-      <Route path='cart' element={<Cart/>}/>
-      <Route path='checkout' element={<Checkout/>}/>
-      <Route path='login' element={<Login/>}/>
-      <Route path='signup' element={<Signup/>}/>
+      <Route path='/' element={<Navigate to='home' />} />
+      <Route path='home' element={<Home />} />
+      <Route path='shop' element={<Shop />} />
+      <Route path='shop/:id' element={<ProductDetails />} />
+      <Route path='cart' element={<Cart />} />
+      <Route path='checkout' element={<ProtectedRoute>
+        <Checkout />
+      </ProtectedRoute>} />
+      <Route path='login' element={<Login />} />
+      <Route path='signup' element={<Signup />} />
     </Routes>
   )
 }
