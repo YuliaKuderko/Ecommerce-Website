ce1640d4283c9fec4f770980d02db2bf79b8384b
Yulia Kuderko <yulia2232@gmail.com>
2023-05-17 12:24:47 +0300

    added dashboard page and router

---
+++ src/admin/Dashboard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import '../styles/dashboard.css';
+import useGetData from '../custom-hooks/useGetData';
+
+function Dashboard() {
+  const {data: products} = useGetData('products');
+  const {data: users} = useGetData('users');
+
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg='3'>
+            <div className="revenue-box">
+              <h5>Total Sales</h5>
+              <span>$7890</span>
+            </div>
+          </Col>
+          <Col lg='3'>
+            <div className="orders-box">
+              <h5>Orders</h5>
+              <span>789</span>
+            </div>
+          </Col>
+          <Col lg='3'>
+            <div className="products-box">
+              <h5>Total Products</h5>
+              <span>{products.length}</span>
+            </div>
+          </Col>
+          <Col lg='3'>
+            <div className="users-box">
+              <h5>Total Users</h5>
+              <span>{users.length}</span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
+
+export default Dashboard
--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -10,6 +10,7 @@ import ProtectedRoute from './ProtectedRoute';
 import AddProducts from '../admin/AddProducts';
 import AllProducts from '../admin/AllProducts';
 import Dashboard from '../admin/Dashboard';
+import Users from '../admin/Users';
 
 function Routers() {
   return (
@@ -24,6 +25,7 @@ function Routers() {
         <Route path='dashboard' element={<Dashboard />} />
         <Route path='dashboard/all-products' element={<AllProducts />} />
         <Route path='dashboard/add-product' element={<AddProducts />} />
+        <Route path='dashboard/users' element={<Users />} />
       </Route>
       <Route path='login' element={<Login />} />
       <Route path='signup' element={<Signup />} />
