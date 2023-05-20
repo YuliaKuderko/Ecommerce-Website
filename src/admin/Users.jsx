157caa10580bee3de8b6f4ac48dc03b6f7471219
Yulia Kuderko <yulia2232@gmail.com>
2023-05-20 16:34:03 +0300

    added conainer tag for margin

--- src/admin/Users.jsx
+++ src/admin/Users.jsx
@@ -12,36 +12,39 @@ function Users() {
         await deleteDoc(doc(db, 'users', id));
         toast.success('User deleted!')
     }
+    
     return (
         <section>
-            <Row>
-                <Col lg='12'><h4 className='fw-bold'>Users</h4></Col>
-                <Col lg='12' className='pt-5'>
-                    <table className='table'>
-                        <thead>
-                            <tr>
-                                <td>Image</td>
-                                <td>Username</td>
-                                <td>Email</td>
-                                <td>Action</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                loading ? <Col lg='12 m-5' className='text-center'><div class="spinner-border" role="status"></div></Col>
-                                    : usersData?.map(user => (
-                                        <tr key={user.uid}>
-                                            <td><img src={user.photoURL} alt='' /></td>
-                                            <td>{user.displayName}</td>
-                                            <td>{user.email}</td>
-                                            <td><button className='btn btn-danger' onClick={() => deleteUser(user.uid)}>Delete</button></td>
-                                        </tr>
-                                    ))
-                            }
-                        </tbody>
-                    </table>
-                </Col>
-            </Row>
+            <Container>
+                <Row>
+                    <Col lg='12'><h4 className='fw-bold'>Users</h4></Col>
+                    <Col lg='12' className='pt-5'>
+                        <table className='table'>
+                            <thead>
+                                <tr>
+                                    <td>Image</td>
+                                    <td>Username</td>
+                                    <td>Email</td>
+                                    <td>Action</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    loading ? <Col lg='12 m-5' className='text-center'><div class="spinner-border" role="status"></div></Col>
+                                        : usersData?.map(user => (
+                                            <tr key={user.uid}>
+                                                <td><img src={user.photoURL} alt='' /></td>
+                                                <td>{user.displayName}</td>
+                                                <td>{user.email}</td>
+                                                <td><button className='btn btn-danger' onClick={() => deleteUser(user.uid)}>Delete</button></td>
+                                            </tr>
+                                        ))
+                                }
+                            </tbody>
+                        </table>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     )
 }
