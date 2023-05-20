8663585030890daa809d9b5ba4793704f6319070
Yulia Kuderko <yulia2232@gmail.com>
2023-05-20 17:17:10 +0300

    dashboard section media queries fixed

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import '../styles/dashboard.css';
 import useGetData from '../custom-hooks/useGetData';
+import { motion } from 'framer-motion';
 
 function Dashboard() {
   const {data: products} = useGetData('products');
@@ -12,28 +13,28 @@ function Dashboard() {
       <Container>
         <Row>
           <Col lg='3'>
-            <div className="revenue-box">
+            <motion.div whileHover={{scale: 1.05}} className="revenue-box">
               <h5>Total Sales</h5>
               <span>$7890</span>
-            </div>
+            </motion.div>
           </Col>
           <Col lg='3'>
-            <div className="orders-box">
+            <motion.div whileHover={{scale: 1.05}} className="orders-box">
               <h5>Orders</h5>
               <span>789</span>
-            </div>
+            </motion.div>
           </Col>
           <Col lg='3'>
-            <div className="products-box">
+            <motion.div whileHover={{scale: 1.05}} className="products-box">
               <h5>Total Products</h5>
               <span>{products.length}</span>
-            </div>
+            </motion.div>
           </Col>
           <Col lg='3'>
-            <div className="users-box">
+            <motion.div whileHover={{scale: 1.05}} className="users-box">
               <h5>Total Users</h5>
               <span>{users.length}</span>
-            </div>
+            </motion.div>
           </Col>
         </Row>
       </Container>
